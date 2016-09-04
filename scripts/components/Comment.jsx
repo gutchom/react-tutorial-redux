f7db67a96a2b1ac07bd9c58813c25e84483a279a
gutchom <yskz.tg@gmail.com>
2016-09-04 20:12:48 +0900

    Adding Markdown

--- scripts/components/Comment.jsx
+++ scripts/components/Comment.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Remarkable from 'remarkable'
 
 class Comment extends React.Component {
-  render() {
+  rawMarkup() {
     let md = new Remarkable()
+    let rawMarkup = md.render(this.props.children.toString())
+    return { __html: rawMarkup }
+  }
+  render() {
     return (
       <div className='comment'>
         <h2 className='comment__author'>
           {this.props.author}
         </h2>
-        {md.render(this.props.children.toString())}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     )
   }
