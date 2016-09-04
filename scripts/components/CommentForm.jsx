d3d54cb2cf904688cf99a9b7f41a35a6d7c8cb14
gutchom <yskz.tg@gmail.com>
2016-09-04 21:31:45 +0900

    Adding new comments

--- scripts/components/CommentForm.jsx
+++ scripts/components/CommentForm.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      author: '',
+      text: ''
+    }
+  }
+  handleAuthorChange(e) {
+    this.setState({ author: e.target.value })
+  }
+  handleTextChange(e) {
+    this.setState({ text: e.target.value })
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    let author = this.state.author.trim()
+    let text = this.state.text.trim()
+    if(!text || !author) {
+      return
+    }
+    this.props.onCommentSubmit({ author: author, text: text })
+    this.setState({ author: '', text: '' })
+  }
   render() {
     return(
-      <div className='comment__form'>
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className='comment__form' onSubmit={e => this.handleSubmit(e)}>
+        <input
+          type='text'
+          placeholder='Your name'
+          value={this.state.author}
+          onChange={e => this.handleAuthorChange(e)}
+        />
+        <input
+          type='text'
+          placeholder='Say something...'
+          value={this.state.text}
+          onChange={e => this.handleTextChange(e)}
+        />
+        <input type='submit' value='Post' />
+      </form>
     )
   }
 }
--- scripts/index.jsx
+++ scripts/index.jsx
@@ -20,12 +20,24 @@ class CommentBox extends React.Component {
       .then( res => this.setState({ data: res.data }) )
       .catch( err => console.log(err) )
   }
+  handleCommentSubmit(comment) {
+    comment.id = Date.now()
+    let comments = this.state.data
+    let newComments = [...comments, comment]
+    this.setState({ data: newComments })
+    axios.post(this.props.url, comment)
+      .then( res => this.setState({ data: res.data }) )
+      .catch( err => {
+        this.setState({ data: comments })
+        console.log(err)
+      })
+  }
   render() {
     return (
       <div className='comment__box'>
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm/>
+        <CommentForm onCommentSubmit={comment => this.handleCommentSubmit(comment)} />
       </div>
     )
   }
